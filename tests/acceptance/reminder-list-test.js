cf45abf41c9e5c275e3054e29e31231fe10d216a
Hugh Morrison <hmorri32@gmail.com>
2017-04-05 16:22:51 -0600

    write these yung acceptance tests for edit issue

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -62,4 +62,41 @@ test('should display message to create reminder if no reminders', function(asser
   })
 })
 
+  test('these yung reminders should be editable', function(assert) {
+    server.createList('reminder', 5)
+
+    visit('/reminders')
+
+    andThen(function() {
+      assert.equal(Ember.$('.reminder-item').length, 5);
+    })
+
+    click('.new-reminder-button')
+
+    fillIn('.input-title', 'asdf')
+    fillIn('.input-body', 'poop')
+
+    click('.submit-button')
+
+    andThen(function() {
+      assert.equal(Ember.$('.reminder-item').length, 6);
+    })
+
+    click('.reminder-item:first');
+    click('.edit-reminder-button');
+
+    fillIn('.input-title', 'suh')
+    fillIn('.input-body', 'bro')
+
+    click('.submit-button')
+
+    andThen(function() {
+      assert.equal(currentURL(), '/reminders');
+      assert.equal(Ember.$('.reminder-item:first').text().trim(), 'suh');
+    });
+
+
+
+  })
+
 
